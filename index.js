d8649916bde305586b94824bb37728e8ad78031c
SharonHuang <s89huang@uwaterloo.ca>
2025-06-17 15:20:52 -0700

    Fix CORS: add https for Netlify origin

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "spectacular-faloodeh-27b361.netlify.app"] 
+    origin: ["http://localhost:5173", "https://spectacular-faloodeh-27b361.netlify.app"] 
   })
  );
  
